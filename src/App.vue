9312c7470e27b926b9c95a983747a9096ec76bf3
zhaohaojun <zhaohaojun@yzwec.corp>
2021-12-08 11:17:39 +0800

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,53 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="less">
+
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family:"Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #2c502f;
+  
 }
 
-#nav {
-  padding: 30px;
+</style>
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+<script>
+export default {
+  
+  oncreate(){
+    this.initWebSocket();
+  },
+  methods:{
 
-    &.router-link-exact-active {
-      color: #42b983;
+    initWebSocket(){
+      let app = this;
+      if("WebSocket" in window){
+        app.ws = new WebSocket('ws://');
+        app.global.setWs(app.ws);
+        app.ws.onopen = function(){
+          console.log("websocket与服务器连接成功");
+        };
+        app.ws.onclose = function(){
+          console.log("websocket与服务器连接已关闭");
+          //尝试重新连接
+          setTimeout(()=>{
+            app.initWebSocket();
+          },5000);
+        }
+
+      }else{
+        console.log("浏览器不支持WebSocket!");
+        
+      }
     }
   }
+
 }
-</style>
+</script>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import global from './js/webSocket.js'
 
+Vue.use(ElementUI)
 Vue.config.productionTip = false
+Vue.prototype.global = global
+
 
 new Vue({
   router,
