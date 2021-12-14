a31795886a102083db00d0618ff0ad05299c8817
zhaohaojun <zhaohaojun@yzwec.corp>
2021-12-14 17:53:14 +0800

    update:完善用户登录

--- src/assets/js/webSocket.js
+++ src/assets/js/webSocket.js
@@ -12,36 +12,36 @@ let webSocketonCloseCallback = null;
  * @param {string} addresss 服务器地址
  * @param {Number} port 服务器端口
  */
-function initWebSocket(addresss,port,onOpenCallback,onCloseCallback){
-  if(addresss === null || addresss === '' || addresss === undefined){
+function initWebSocket(addresss, port, onOpenCallback, onCloseCallback) {
+  if (addresss === null || addresss === '' || addresss === undefined) {
     throw new Error('服务器地址不能为空');
   }
-  if(port === null || port === 0 || port === undefined){
+  if (port === null || port === 0 || port === undefined) {
     throw new Error('服务器端口不能为空');
   }
-  if(onOpenCallback === null || onOpenCallback === undefined){
+  if (onOpenCallback === null || onOpenCallback === undefined) {
     throw new Error('回调函数不能为空');
   }
-  if(onCloseCallback === null || onCloseCallback === undefined){
+  if (onCloseCallback === null || onCloseCallback === undefined) {
     throw new Error('回调函数不能为空');
   }
   wsaddress = addresss;
   wsport = port;
-  if("WebSocket" in window){
-    ws = new WebSocket('ws://' + wsaddress + ':' + wsport +'');
-    ws.onopen = function(){
+  if ("WebSocket" in window) {
+    ws = new WebSocket('ws://' + wsaddress + ':' + wsport + '');
+    ws.onopen = function () {
       webSocketonOpenCallback = onOpenCallback;
       webSocketonOpenCallback();
     };
-    ws.onclose = function(){
+    ws.onclose = function () {
       webSocketonCloseCallback = onCloseCallback;
       webSocketonCloseCallback();
     };
-    ws.onmessage = function(data){
+    ws.onmessage = function (data) {
       receivedMessage(data);
     }
 
-  }else{
+  } else {
     throw new Error("浏览器不支持WebSocket");
   }
 }
@@ -50,20 +50,20 @@ function initWebSocket(addresss,port,onOpenCallback,onCloseCallback){
  * @author Jaiden
  * @description 重试连接
  */
-function reTryConnect(){
-  
-  if("WebSocket" in window){
-    ws = new WebSocket('ws://' + wsaddress + ':' + wsport +'');
-    ws.onopen = function(){
+function reTryConnect() {
+
+  if ("WebSocket" in window) {
+    ws = new WebSocket('ws://' + wsaddress + ':' + wsport + '');
+    ws.onopen = function () {
       webSocketonOpenCallback();
     };
-    ws.onclose = function(){
+    ws.onclose = function () {
       webSocketonCloseCallback();
     };
-    ws.onmessage = function(data){
+    ws.onmessage = function (data) {
       receivedMessage(data);
     }
-  }else{
+  } else {
     throw new Error("浏览器不支持WebSocket");
   }
 }
@@ -73,7 +73,7 @@ function reTryConnect(){
  * @description 注册信息获取回调
  * @param {function} func 
  */
-function addMessageCallback(func){
+function addMessageCallback(func) {
   webSocketonMessageCallback.push(func);
 }
 
@@ -82,26 +82,32 @@ function addMessageCallback(func){
  * @description 发送数据
  * @param {JSONObject} data 
  */
-function sendMessage(data){
+function sendMessage(data) {
   ws.send(JSON.stringify(data));
 }
 
 /**
  * @author Jaiden
  * @description 接受处理数据[内部使用]
- * @param {string} data 
+ * @param {MessageEvent} message 
  */
-function receivedMessage(data){
-  if(onMessageCallback.length > 0){
-    var obj = JSON.parse(data);
-    for(item in onMessageCallback){
-      item(obj);
+function receivedMessage(message) {
+
+  if (message.data === undefined || message.data === '')
+    return;
+
+  if (webSocketonMessageCallback.length > 0) {
+    var obj = JSON.parse(message.data);
+    for (var i = 0; i < webSocketonMessageCallback.length; i++) {
+      var callback = webSocketonMessageCallback[i];
+      callback(obj);
     }
+
   }
 }
 
 
-export default{
+export default {
   initWebSocket,
   reTryConnect,
   addMessageCallback,
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,113 +1,108 @@
 <template>
-    <div class="loginContainer">
-        <div class="loginBox">
-            <h1>AirplaneCrash</h1>
-            <el-form ref="User" :model="User" :rules="rules" label-width="60px">
-                <el-form-item label="昵称" prop="nickname">
-                    <el-input type="text" v-model="User.nickname"></el-input>
-                </el-form-item>
-                <el-button type="primary" @click="login('User')">登录</el-button>
-                <el-button @click="reset('User')">重置</el-button>
-            </el-form>
-        </div>
-    </div>
+  <div class="loginContainer">
+    <el-card class="box-card loginBox">
+      <div class="el-card__header">
+        <span style="color: rgb(68, 68, 68); font-size: 22px"
+          >AirplaneCrash</span
+        >
+      </div>
+      <div class="el-card__body">
+        <el-form ref="User" :model="User" :rules="rules" label-width="60px">
+          <el-form-item label="昵称" prop="nickname">
+            <el-input type="text" v-model="User.nickname"></el-input>
+          </el-form-item>
+          <el-button type="primary" @click="login('User')">登录</el-button>
+          <el-button @click="reset('User')">重置</el-button>
+        </el-form>
+      </div>
+    </el-card>
+  </div>
 </template>
 
 <style scoped>
-
-html,body{
-    margin:0;
-    padding:0;
-    position: relative;
-  
+html,
+body {
+  margin: 0;
+  padding: 0;
+  position: relative;
 }
 
-.loginContainer{
-    width:100%;
-    height:100%;
-
+.loginContainer {
+  width: 100%;
+  height: 100%;
 }
 
-.loginBox{
-    position: absolute;
-    top:50%;
-    left:50%;
-    margin-top:-150px;
-    margin-left:-175px;
-    width: 360px;
-    min-height: 300px;
-    padding: 30px 20px 20px;
-    border-radius: 8px;
-    box-sizing: border-box;
-    background-color: #fff;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
-    text-align: center;
+.loginBox {
+  width: 380px;
+  margin-top: 120px;
+  margin-left: 120px;
 }
-
-
-
 </style>
 
 <script>
+import App from "../App.vue";
 export default {
-    data(){
-        return{
-            User:{
-                sysno:0,
-                nickname:'',
-                battleScore:0
-            },
-            rules:{
-                nickname:[
-                    {required:true,message:'请输入用户昵称',trigger:'blur'},
-                    {min:3,max:8,message:'长度在3到8个字符',trigger:'blur'},
-                ]
-            }
-        }
-    },
-    created(){
-        this.webSocket.addMessageCallback(this.onRreceived);
+  components: { App },
+  data() {
+    return {
+      User: {
+        sysno: 0,
+        nickname: "",
+        battleScore: 0,
+      },
+      rules: {
+        nickname: [
+          { required: true, message: "请输入用户昵称", trigger: "blur" },
+          { min: 3, max: 8, message: "长度在3到8个字符", trigger: "blur" },
+        ],
+      },
+    };
+  },
+  created() {
+    this.webSocket.addMessageCallback(this.onRreceived);
+  },
+  methods: {
+    onRreceived: function (msg) {
+      console.log(msg);
+      if (msg === undefined || msg === null) return;
+      if (msg.Code == 1) {
+        //登录处理
+        var userinfo = JSON.parse(msg.Message);
+        console.log("登录成功");
+        console.log(userinfo);
+        this.cookie.setCookie("UserInfo", msg.Message);
+        //登录成功跳转页面
 
+        let loadingInstance = this.$loading({
+          fullscreen: true,
+          background: "rgba(0, 0, 0, 0.7)",
+        });
+        setTimeout(() => {
+          loadingInstance.close();
+        }, 2000);
+      }
     },
-    methods:{
-        onRreceived:function(msg){
-            console.log(msg);
-            this.cookie.setCookie('UserInfo',JSON.stringify(msg));
-        },
-        reset:function(User){
-            this.$refs[User].resetFields();
-        },
-        login:function(User){
-            this.$refs[User].validate((valid)=>{
-                if(valid){
-                    
-                    var data = {};
-                    data.Code = 1;
-                    data.Message = {};
-
-                    data.Message.NickName = this.User.nickname;
-                    this.webSocket.sendMessage(data);
-                    
-                    return;
-                    let v = this;
-                    const loading = v.$loading({
-                        lock:true,
-                        text:'Loading',
-                        spinner:'el-icon-loading'
-                    });
-                    setTimeout(() => {
-                        loading.close();
-                    }, 2000);
-                    v.$message({
-                        type:'success',
-                        message:'login success'
-                    });
-                }else{
-                    console.log('login error');
-                    return false;
-                }
-            });
+    reset: function (User) {
+      this.$refs[User].resetFields();
+    },
+    login: function (User) {
+      this.$refs[User].validate((valid) => {
+        if (valid) {
+          let loadingInstance = this.$loading({
+            lock: true,
+            text: "登录中",
+            fullscreen: true,
+            background: "rgba(0, 0, 0, 0.7)",
+          });
+          var data = {};
+          data.Code = 1;
+          var userdata = {};
+          userdata.NickName = this.User.nickname;
+          data.Message = JSON.stringify(userdata);
+          this.webSocket.sendMessage(data);
         }
-    }
-}
+      });
+    },
+  },
+};
 </script>
